ba8d064c653f52bd3cd9e43224393fae7cbd93c5
ksanjaykumar1 <ksanjayk@protonmail.com>
2022-05-16 02:00:41 +0530

    middleware for authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,4 +4,26 @@ const config =require('config')
 
 module.exports = function (req,res, next){
 
+    // Get token from header
+
+    const token = req.heade('x-auth-token')
+
+    // Check if token is present
+
+    if(!token){
+        return res.status(401).json({msg: 'No token, authorization denied'})
+    }
+
+    // verify to token 
+    try{
+        // decode the token
+        const decoded = jwt.verify(token,config.get('jwtSecret'))
+
+        // set req.user
+        req.user = decoded.user;
+        next();
+    }
+    catch(err){
+        req.status(401).json({msg: 'Token is not valid'})
+    }
 }
