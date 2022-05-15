3dce1bf5f0e441f75791ebfc4f84480bc841914b
ksanjaykumar1 <ksanjayk@protonmail.com>
2022-05-16 00:39:22 +0530

    intital routes

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+
+
+// @route       POST api/users
+// @desc        Register user
+// @access      Public
+router.post('/',(req,res)=> {
+    
+    res.send('User adac route')
+})
+
+
+module.exports = router
