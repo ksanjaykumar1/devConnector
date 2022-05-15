efec3c889a34e2aeeee9f2631356dc458bedc725
ksanjaykumar1 <ksanjayk@protonmail.com>
2022-05-16 01:23:45 +0530

    hash password using bcrypt and save user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const gravatar = require('gravatar');
+const bcrypt = require("bcryptjs")
 
 const User = require('../../models/User')
 
@@ -44,7 +45,22 @@ router.post('/',[
         d: 'mm'
     })
 
+    user  = new User({
+        name,
+        email,
+        avatar,
+        password
+    })
+
     // Encrypt password
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password,salt)
+
+    // save user in database
+
+    await user.save();
 
     // Return jsonwebtoken
 
