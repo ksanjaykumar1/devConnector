b44858a7a4c29ded30808d3b9578dc10b5eefa7d
ksanjaykumar1 <ksanjayk@protonmail.com>
2022-05-16 01:16:22 +0530

    check if user exists in db

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const {check, validationResult} = require('express-validator')
 
+const User = require('../../models/User')
+
 // @route       POST api/users
 // @desc        Register user
 // @access      Public
@@ -13,12 +15,40 @@ router.post('/',[
       'password',
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
-],(req,res)=> {
+],async (req,res)=> {
 
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array()})
     }
+
+    const { name, email, password}= req.body
+
+    try{
+
+    // See if user exists
+
+    let user = await User.findOne({email:email})
+
+    if(user){
+
+        // sending array of errors 
+        res.status(400).json({errors:[{ msg: "User already exists"}]});
+    }
+
+    // Get users gravatar
+
+    // Encrypt password
+
+    // Return jsonwebtoken
+
+    }catch(error){
+
+        console.error(err.message)
+        res.status(500),send('Serer error')
+    }
+    
+
 
     res.send('User adac route')
 })
